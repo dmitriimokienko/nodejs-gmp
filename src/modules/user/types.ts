00dcf486c5ba579c5a3dd0d0a52099b9cc5c4fa1
dmitriimokienko <dmitrymokienko@gmail.com>
2020-01-27 20:58:23 +0400

    Clear legacy code

--- src/modules/user/types.ts
+++ src/modules/user/types.ts
@@ -2,9 +2,3 @@ import { Model } from 'sequelize';
 import { UserModel } from './model';
 
 export type UserModelType = typeof Model & typeof UserModel;
-
-export type BodyType = {
-    login: string;
-    password: string;
-    age: number;
-};
