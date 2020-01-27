fb2ef5cbf9e86b2a5148b8abf913800add49947e
dmitriimokienko <dmitrymokienko@gmail.com>
2020-01-27 20:28:17 +0400

    Clean project

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,3 @@ export const config = {
     host,
     port
 };
-
-export enum StatusCode {
-    BadRequest = 400,
-    NotFound = 404,
-    Conflict = 409,
-    UnprocessableEntity = 422,
-    Created = 201,
-    Ok = 200,
-    NoContent = 204
-}
