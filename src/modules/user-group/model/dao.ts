d2d60aee6f305ca19b0fd30103c35e1d65288a72
Dmitrii_Mokienko <dmitrii_mokienko@epam.com>
2020-01-31 18:18:13 +0400

    Add UserGroup

---
+++ src/modules/user-group/model/dao.ts
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../../../../resources';
+import { UserModel } from '../../user/model';
+import { GroupModel } from '../../group/model';
+
+const attributes = {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    userId: {
+        type: DataTypes.UUID,
+        validate: { isUUID: 4 },
+        references: {
+            model: UserModel,
+            key: 'id'
+        }
+    },
+    groupId: {
+        type: DataTypes.UUID,
+        validate: { isUUID: 4 },
+        references: {
+            model: GroupModel,
+            key: 'id'
+        }
+    }
+};
+
+const options = {
+    sequelize,
+    modelName: 'UserGroup',
+    tableName: 'UsersGroups'
+};
+
+export class UserGroupModel extends Model<UserGroupModel> {
+    public readonly id?: string;
+    public readonly userId!: string;
+    public readonly groupId!: string;
+}
+
+UserGroupModel.init(attributes, options);
+
+// TODO: may be relocate to another places
+UserModel.belongsToMany(GroupModel, { through: UserGroupModel });
+GroupModel.belongsToMany(UserModel, { through: UserGroupModel });
