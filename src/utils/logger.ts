9a5a7259e87fae71f941b05a3a87fe890d796e28
Dmitrii_Mokienko <Dmitrii_Mokienko@epam.com>
2020-02-20 12:02:17 +0400

    Fix logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,30 +5,15 @@ const formatter = format.printf(({ level, message, timestamp }: TransformableInf
     return `${timestamp} [${level}]: ${message}`;
 });
 
-// const levels = {
-//     error: 0,
-//     warn: 1,
-//     info: 2
-// };
-
-// const colors = {
-// //     error: 'red',
-// //     warn: 'yellow',
-// //     info: 'cyan'
-// // };
-// //
-// // addColors(colors);
-
 const { combine, timestamp, colorize } = format;
 
 export const logger = createLogger({
-    // levels: config.npm.levels,
+    exitOnError: false,
     format: combine(timestamp(), formatter),
     transports: [
         new transports.Console({ format: combine(colorize(), timestamp(), formatter) }),
         new transports.File({ filename: 'logs/errors.log', level: 'error' }),
         new transports.File({ filename: 'logs/warnings.log', level: 'warn' }),
         new transports.File({ filename: 'logs/info.log', level: 'info' })
-    ],
-    exitOnError: false
+    ]
 });
