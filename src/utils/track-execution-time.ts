9b65d3e9c5b13fd6da5fafa4edc8adee8c4e264d
Dmitrii_Mokienko <Dmitrii_Mokienko@epam.com>
2020-02-25 11:50:45 +0400

    Little fixies

--- src/utils/track-execution-time.ts
+++ src/utils/track-execution-time.ts
@@ -3,19 +3,19 @@ import { logger } from './logger';
 const NS_PER_SEC = 1e9;
 const MS_PER_NS = 1e6;
 
-const calculateExecutionTime = (diff: number[]): number => (diff[0] * NS_PER_SEC + diff[1]) / MS_PER_NS;
+const calculateExecutionTime = (time: number[]): number => (time[0] * NS_PER_SEC + time[1]) / MS_PER_NS;
 
 export function trackExecutionTime(_target: any, propertyName: string, descriptor: TypedPropertyDescriptor<any>) {
     const method: Function | undefined = descriptor.value;
 
-    descriptor.value = async function trackTime(...args: any) {
+    descriptor.value = async function execute(...args: any) {
         const time = process.hrtime();
 
         const result = method && (await method.apply(this, args));
 
-        const diff = process.hrtime(time);
+        const timeDifference = process.hrtime(time);
 
-        logger.info(`'${propertyName}' method execute ${calculateExecutionTime(diff)} ms`);
+        logger.info(`'${propertyName}' method execute ${calculateExecutionTime(timeDifference)} ms`);
 
         return result;
     };
